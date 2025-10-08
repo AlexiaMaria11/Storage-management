cbd2d602ca08c0261c7de57d6db73d2fbf8c837d
AlexiaMaria11 <oprtitamariaalexia@gmail.com>
2025-10-08 18:46:19 +0300

    Implement Sign-up

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -59,8 +59,7 @@ export const createAccount = async ({
       {
         fullName,
         email,
-        avatar:
-          "https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg",
+        avatar: avatarPlaceholderUrl,
         accountId,
       }
     );
